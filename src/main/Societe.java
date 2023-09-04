d3916d5ad711834247cb021cbf23c6339f401e1a
Maxime Blot <maxime.blot.etu@univ-lille.fr>
2023-09-04 16:16:12 +0200

    j'ai fait Societe.java

---
+++ src/main/Societe.java
@@ -0,0 +1,42 @@
+package main;
+
+public enum Societe{
+    UberEats(1.4, 1.2, 0.5, 0.6, 1), //Guerrier
+    Deliveroo(0.8, 0.9, 1.5, 1.1, 1.2),//Magicien
+    KingDelivery(1.2, 0.65, 1, 1.5, 1.6),//Voleur
+    Indépendant(1, 1, 1, 1, 1); //Vierge
+
+    private double atkMult;
+    private double defMult;
+    private double manaMult;
+    private double stealthMult;
+    private double speedMult;
+
+    private Societe(double atkMult, double defMult, double manaMult, double stealthMult, double speedMult){
+        this.atkMult = atkMult;
+        this.defMult = defMult;
+        this.manaMult = manaMult;
+        this.stealthMult = stealthMult;
+        this.speedMult = speedMult;
+    }
+
+    public double getAtkMult(){
+        return this.atkMult;
+    }
+
+    public double getDefMult(){
+        return this.defMult;
+    }
+
+    public double getManaMult(){
+        return this.manaMult;
+    }
+
+    public double getStealhMult(){
+        return this.stealthMult;
+    }
+
+    public double getSpeedMult(){
+        return this.speedMult;
+    }
+}
