4a47c1cbdf41f4e75288f9370e69ded5ffc934dc
Manon Leclercq <manon.leclercq7.etu@univ-lille.fr>
2023-09-05 09:51:05 +0200

    added presentation of the inventory in jeu.java

--- src/main/Jeu.java
+++ src/main/Jeu.java
@@ -32,6 +32,10 @@ public class Jeu {
         System.out.println();
         System.out.println("Ok " + perso.getName() + ", voici vos statistiques : ");
         System.out.println(perso);
+        perso.addItem(Item.BATON);
+        perso.addItem(Item.KEBAB);
+        perso.addItem(Item.CHAUSSETTES);
+        System.out.println(perso.seeInventory());
         sc.close();
     }
 
