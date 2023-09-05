5c054bd4d1204d994c50082276bd73b056b80872
Simon Hayart <simon.hayart.etu@univ-lille.fr>
2023-09-05 09:22:56 +0200

    Event

--- src/main/Jeu.java
+++ src/main/Jeu.java
@@ -1,8 +1,29 @@
 package main;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Jeu {
+    private static List<Item> listeObjet;
+
+     public static void setListeObjet(List<Item> listeObjet) {
+        Jeu.listeObjet = listeObjet;
+    }
+
+    public static void retirerObjet(Item objet){
+        Jeu.listeObjet.remove(objet);
+    }
+
+    public static void ajouterObjet(Item objet){
+        Jeu.listeObjet.add(objet);
+    }
+
+    public static List<Item> getListeObjet() {
+        return listeObjet;
+    }
+
+    
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Entrez votre nom de personnage : ");
@@ -13,4 +34,11 @@ public class Jeu {
         System.out.println(perso);
         sc.close();
     }
+
+
+
+    
+
+
+    
 }
